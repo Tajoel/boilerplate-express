d0ccc8756cab47f1540293245ed266decb4554d2
Prog_Frog <149373350+Tajoel@users.noreply.github.com>
2024-05-06 07:05:38 +0000

    Pending changes exported from your codespace

--- myApp.js
+++ myApp.js
@@ -3,6 +3,14 @@ let app = express();
 
 
 
+  app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+  });
+
+  app.use(express.static(__dirname + "/public"));
+
+
+
 
 
 
